235f7dec364ef3bb520b00f9d78258f6c376040e
JanhonChen <lantize@gmail.com>
2018-08-30 00:53:44 +0800

    Timer function sucess.

--- app/src/main/java/com/example/janhon/running/Running/RunningActivity.java
+++ app/src/main/java/com/example/janhon/running/Running/RunningActivity.java
@@ -32,75 +32,6 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class RunningActivity extends FragmentActivity  {
-    TextView text, text2, text3;
-    long starttime = 0;
-    private Button b;
-    //this  posts a message to the main thread from our timertask
-    //and updates the textfield
-    final Handler h = new Handler(new Handler.Callback() {
-
-        @SuppressLint("DefaultLocale")
-        @Override
-        public boolean handleMessage(Message msg) {
-            long millis = System.currentTimeMillis() - starttime;
-            int seconds = (int) (millis / 1000);
-            int minutes = seconds / 60;
-            seconds     = seconds % 60;
-
-            text.setText(String.format("%d:%02d", minutes, seconds));
-            return false;
-        }
-    });
-    //runs without timer be reposting self
-    Handler h2 = new Handler();
-    Runnable run = new Runnable() {
-
-        @SuppressLint("DefaultLocale")
-        @Override
-        public void run() {
-            long millis = System.currentTimeMillis() - starttime;
-            int seconds = (int) (millis / 1000);
-            int minutes = seconds / 60;
-            seconds     = seconds % 60;
-
-            text3.setText(String.format("%d:%02d", minutes, seconds));
-
-            h2.postDelayed(this, 500);
-        }
-    };
-
-    //tells handler to send a message
-    class firstTask extends TimerTask {
-
-        @Override
-        public void run() {
-            h.sendEmptyMessage(0);
-        }
-    };
-
-    //tells activity to run on ui thread
-    class secondTask extends TimerTask {
-
-        @Override
-        public void run() {
-            RunningActivity.this.runOnUiThread(new Runnable() {
-
-                @SuppressLint("DefaultLocale")
-                @Override
-                public void run() {
-                    long millis = System.currentTimeMillis() - starttime;
-                    int seconds = (int) (millis / 1000);
-                    int minutes = seconds / 60;
-                    seconds     = seconds % 60;
-
-                    text2.setText(String.format("%d:%02d", minutes, seconds));
-                }
-            });
-        }
-    };
-
-
-    Timer timer = new Timer();
 
     private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener //當選項轉換時進入監聽器.
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -210,31 +141,6 @@ public class RunningActivity extends FragmentActivity  {
 //        intent.putExtras(bundle);
         /* 呼叫startActivity()開啟新的頁面 */
         startActivity(intent);
-
-        text = (TextView)findViewById(R.id.text);
-        text2 = (TextView)findViewById(R.id.text2);
-        text3 = (TextView)findViewById(R.id.text3);
-        b = (Button)findViewById(R.id.btSubmit);
-        b.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Button b = (Button)v;
-                if(b.getText().equals("STOP")){
-                    timer.cancel();
-                    timer.purge();
-                    h2.removeCallbacks(run);
-                    // b.setText("start");
-                }else{
-                    starttime = System.currentTimeMillis();
-                    timer = new Timer();
-                    timer.schedule(new RunningActivity.firstTask(), 0,500);
-                    timer.schedule(new RunningActivity.secondTask(),  0,500);
-                    h2.postDelayed(run, 0);
-                    // b.setText("stop");
-                }
-            }
-        });
 
 
     }
